67169fb46e96006648327c411ce18d416fedfc80
Heber Sandoval <he3ers@gmail.com>
2016-06-16 07:20:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-
+  resources :users
+  resources :rides
+  resources :attractions
+  root "welcome#index"
+  get "/signin" => "sessions#new"
+  post "/signin" => "sessions#create"
+  delete "/signout" => "sessions#destroy"
 
 end
